146c345c06dccbf16c65281a5135917aebb1237a
dlopezos1616 <dlopezos1616@users.noreply.github.com>
2026-06-09 02:48:51 +0000

    Limpieza: solo archivos esenciales para Vercel deploy

--- start-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  echo "[$(date)] Starting Next.js production server..."
-  node ./node_modules/.bin/next start -p 3000 2>&1
-  EXIT_CODE=$?
-  echo "[$(date)] Next.js exited with code $EXIT_CODE, restarting in 3 seconds..."
-  sleep 3
-done
